818a0055fc2e5b96ec511e1175cbd7e8327f6e8e
anacarolpnh <anacarol7891@gmail.com>
2023-07-10 21:48:42 -0300

    update jogoteca

---
+++ jogoteca.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template, request, redirect, session, flash
+
+class Jogo:
+    def __init__(self, nome, categoria, console):
+        self.nome=nome
+        self.categoria=categoria
+        self.console=console
+
+jogo1 = Jogo('Tomb Raiber', 'Puzzle', 'Play Station')
+jogo2 = Jogo('Good of War', 'Puzzle', 'Play Station')
+jogo3 = Jogo('Resident Evil', 'Terror', 'Play Station')
+lista = [jogo1, jogo2, jogo3]
+
+app = Flask(__name__)
+app.secret_key = 'teste'
+
+@app.route('/')
+def index():
+    return render_template('lista.html', titulo='Jogos', jogos=lista)
+
+@app.route('/cadastro')
+def cadastro():
+    return render_template('cadastro.html', titulo='Cadastro')
+
+@app.route('/criar', methods=['POST',])
+def criar():
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+    jogo = Jogo(nome, categoria, console)
+    lista.append(jogo)
+    return redirect('/')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/autenticar', methods=['POST', ])
+def autenticar():
+    if 'ana' == request.form['senha']:
+        session['usuario_logado'] = request.form['usuario']
+        flash(session['usuario_logado']+' logado com sucesso')
+        return redirect('/')
+    else:
+        flash('Usuario nao existe')
+        return redirect('/login')
+
+app.run()
